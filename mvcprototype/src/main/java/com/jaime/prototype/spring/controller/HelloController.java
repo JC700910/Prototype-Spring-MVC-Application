1874d306581dbd28ee55d621b4830098784f6482
JC700910 <jaimechester@hotmail.com>
2016-11-10 13:28:38 +0000

    JNDI configuration
    Managed to configure jndi for database connection

--- mvcprototype/src/main/java/com/jaime/prototype/spring/controller/HelloController.java
+++ mvcprototype/src/main/java/com/jaime/prototype/spring/controller/HelloController.java
@@ -1,15 +1,19 @@
 package com.jaime.prototype.spring.controller;
 
+import javax.sql.DataSource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.jaime.prototype.dao.DataSourceObject;
 
 @Controller
 public class HelloController {
     
     
     @GetMapping("/hello")
-    public String hello(Model model){
+    public String hello(Model model, DataSourceObject dataSource){
        model.addAttribute("name","Jaime");
        
        return "welcome";
